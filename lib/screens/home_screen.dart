6233a5314fe9cd9be3b1201798ba452dfd3af79c
Atharv Sharma <atharv228.as@gmail.com>
2020-10-06 18:48:39 +0530

    Added Bottom Sliding Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:designcode/components/lists/recent_course_list.dart';
 import 'package:designcode/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
+import 'continue_watching_screen.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -116,6 +117,9 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               ],
             ),
           ),
+
+            ContinueWatchingScreen(),
+
             IgnorePointer(
               ignoring: sidebarHidden,
               child: Stack(
@@ -160,4 +164,3 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     );
   }
 }
-
